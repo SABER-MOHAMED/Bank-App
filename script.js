847a468dee83fff0187414bba1172779f8655df0
SABER-MOHAMED <95752534+SABER-MOHAMED@users.noreply.github.com>
2022-10-10 00:42:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// BANKING APP
 
 /////////////////////////////////////////////////
 // Data
@@ -10,7 +10,7 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'MOHAMED SABER',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
